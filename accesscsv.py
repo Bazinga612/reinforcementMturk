40ee16a7c9443e44e37ff28fb661ecc19c11e436
arnab64 <arnabborah1@gmail.com>
2017-10-16 16:53:11 -0700

    edited CSV access function

--- accesscsv.py
+++ accesscsv.py
@@ -1,51 +1,65 @@
 import csv
-'''
-with open('tv_movie_combined.csv', 'r') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-	k=0
-	for row in spamreader:
-		print(k,len(row),row)
-'''
+import sqlite3
 
-def combinefunc(arr):			#function to eliminate deformities in the CSV file
-	print("The input array is",arr)
-	stat,addons=0,0
-	for j in range(len(arr)-addons):
-		#print("Stat is",stat,arr[j-addons])
-		if stat==0:
-			#arrord=[ord(p) for p in arr[j]]
-			if ord(arr[j-addons][0])==34:
-				stat=1
-				if ord(arr[j-addons][-1])!=34:
-					tempstr=arr[j-addons][1:]
-					#print("case1")
-				else:
-					arr[j-addons]=arr[j-addons][1:-1]
-					#print("case2")
-					continue;
-			else:
+class csvprocessing:
+	def __init__(self):
+		conn = sqlite3.connect('rlmturk.db')
+		self.c = conn.cursor()
+		self.labels=None
+		self.commands=[]
+
+	def initialize_database(self):
+		#initialize table 'dialog_acts'
+		self.commands.append("CREATE TABLE dialog_acts (date text, trans text, symbol text, qty real, price real)")
+		self.commands.append("")
+
+	def properprint(self,arx):
+		for j in range(len(arx)):
+			print(j,arx[j])
+
+	def processcsv(self):
+		with open('tv_movie_combined.csv', 'r') as csvfile:
+			spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+			k,lmt=0,100
+
+			for row in spamreader:
+				rowx=self.combinefunc(row)
+				if k==0:
+					self.labels=rowx
+				#print(len(rowx),rowx)
+				#self.properprint(rowx)
+				k+=1
+				if k==lmt:
+					break;
+			print("Labels",self.labels)
+
+	def combinefunc(self,arr):
+		#print("\nThe input array is",arr)
+		stat,addons=0,0
+		for j in range(len(arr)-addons):
+			#print("Stat is",stat,arr[j-addons])
+			if len(arr[j-addons])==0:
 				continue;
-		elif stat==1:
-			tempstr+=', '
-			if ord(arr[j-addons][-1])!=34:
-				tempstr+=arr[j]
-				arr[j-1-addons]=tempstr
-				arr.pop(j-addons)
-				#print("case3")
 			else:
-				tempstr+=arr[j-addons][:-1]
-				arr[j-1-addons]=tempstr
-				arr.pop(j-addons)
-				stat=0
-				#print("case4")
-			addons+=1
-		#print("tempstr=",tempstr)
-		#print("arr=",arr)
-	print("The output array is",arr)
+				temparrx=[ord(f) for f in arr[j-addons]]
+				if stat==0:
+					if temparrx.count(34)%2==1:
+						tempstr=arr[j-addons] if temparrx[0]!=34 else arr[j-addons][1:]
+						stat=1
+					else: 
+						continue;
+				elif stat==1:
+					tempstr+=', '
+					tempstr+=arr[j-addons]
+					arr[j-1-addons]=tempstr
+					arr.pop(j-addons)
+					if temparrx.count(34)%2==1:	
+						stat=0						
+					addons+=1
+			#print("tempstr=",tempstr)
+			#print("arr=",arr)
+		#print("The output array is",arr)
+		return arr		
 
-
-testcases=[['"hey','this is','insane"','"i know right"'],
-['"hey','this is insane"','"i know right"']
-]
-for el in testcases:
-	combinefunc(el)
+csvp = csvprocessing()
+csvp.processcsv()
