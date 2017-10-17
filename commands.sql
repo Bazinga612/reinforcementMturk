2e28de7157fcc82309818fb77a9e6ea4e270982d
arnab64 <arnabborah1@gmail.com>
2017-10-17 00:56:09 -0700

    SQL commands generated

--- commands.sql
+++ commands.sql
@@ -9,83 +9,83 @@ CREATE TABLE dialog_act4 (dialog_act4_id int NOT NULL PRIMARY KEY, utterance tex
 
 
 #Dialog acts table commands
-INSERT INTO dialog_acts VALUES (0, Answer.negative);
-INSERT INTO dialog_acts VALUES (3, Answer.continuation);
-INSERT INTO dialog_acts VALUES (7, Answer.positive);
-INSERT INTO dialog_acts VALUES (2, Answer.change);
-INSERT INTO dialog_acts VALUES (1, Answer.TalkMore);
-INSERT INTO dialog_acts VALUES (4, Answer.joking_sarcastic);
-INSERT INTO dialog_acts VALUES (5, Answer.elaboration);
-INSERT INTO dialog_acts VALUES (6, Answer.rhetorical);
+INSERT INTO dialog_acts VALUES (6, "Answer.rhetorical");
+INSERT INTO dialog_acts VALUES (0, "Answer.negative");
+INSERT INTO dialog_acts VALUES (7, "Answer.positive");
+INSERT INTO dialog_acts VALUES (5, "Answer.elaboration");
+INSERT INTO dialog_acts VALUES (4, "Answer.joking_sarcastic");
+INSERT INTO dialog_acts VALUES (1, "Answer.TalkMore");
+INSERT INTO dialog_acts VALUES (3, "Answer.continuation");
+INSERT INTO dialog_acts VALUES (2, "Answer.change");
 
 
 #Starter table commands
-INSERT INTO starter VALUES (1, "I STILL think ""Monty Python and the Holy Grail"" is funny.");
-INSERT INTO starter VALUES (0, """The Wizard of Oz"" and ""Gone with the Wind"" were amazing at the time in part because of the color cinematography.");
+INSERT INTO starter VALUES (1, "I STILL think ""Monty Python and the Holy Grail"" is funny.") 
+INSERT INTO starter VALUES (0, """The Wizard of Oz"" and ""Gone with the Wind"" were amazing at the time in part because of the color cinematography.") 
 
 
 #Dialog acts1 table commands
-INSERT INTO dialog_act1 VALUES (4, "What do you like about these movies the best. The idea that they end on such a positive note all the time? I kind of like that too. Tell me why you like the endings?", 1);
-INSERT INTO dialog_act1 VALUES (10, "I agree,  the color film was a game-changer at the time.", 7);
-INSERT INTO dialog_act1 VALUES (11, "Who wasn't blown away by these movies?", 6);
-INSERT INTO dialog_act1 VALUES (13, "Some of their skits were pretty obscure due to their addictions I think.", 0);
-INSERT INTO dialog_act1 VALUES (6, "I love movies like this. They are amazing. I wish I could see more like this in these days.", 7);
+INSERT INTO dialog_act1 VALUES (8, "Who can honestly say they don't know who Dorothy or the Tin Man is?  These movies are memorable for a reason!", 6);
 INSERT INTO dialog_act1 VALUES (12, "Oh come on,  that movie is for nerds,  losers who live in their mom's basement.", 0);
+INSERT INTO dialog_act1 VALUES (10, "I agree,  the color film was a game-changer at the time.", 7);
+INSERT INTO dialog_act1 VALUES (4, "What do you like about these movies the best. The idea that they end on such a positive note all the time? I kind of like that too. Tell me why you like the endings?", 1);
 INSERT INTO dialog_act1 VALUES (0, "I think these type of movies are overrated because they are old. Don't you think that is the reason they are so popular?", 0);
-INSERT INTO dialog_act1 VALUES (2, "What do you think made ""Gone with the Wind"" seem authentic?", 1);
+INSERT INTO dialog_act1 VALUES (13, "Some of their skits were pretty obscure due to their addictions I think.", 0);
+INSERT INTO dialog_act1 VALUES (11, "Who wasn't blown away by these movies?", 6);
 INSERT INTO dialog_act1 VALUES (7, "I really can't stand watching ""The Wizard of Oz.""  It was good when I was a kid,  but I grew out of it.", 0);
-INSERT INTO dialog_act1 VALUES (3, "What do you think is the better movie? Its not cliche to ask if its Gone with the Wind or Wizard of Oz at all.", 6);
-INSERT INTO dialog_act1 VALUES (1, "Yeah. What else did these films revolutionize about the industry?", 1);
 INSERT INTO dialog_act1 VALUES (5, "I don't think they look very good by today's standards,  of course.", 0);
 INSERT INTO dialog_act1 VALUES (9, "I love ""The Wizard of Oz!""  There aren't many other movies that have the same longivity.", 7);
-INSERT INTO dialog_act1 VALUES (8, "Who can honestly say they don't know who Dorothy or the Tin Man is?  These movies are memorable for a reason!", 6);
+INSERT INTO dialog_act1 VALUES (2, "What do you think made ""Gone with the Wind"" seem authentic?", 1);
+INSERT INTO dialog_act1 VALUES (1, "Yeah. What else did these films revolutionize about the industry?", 1);
+INSERT INTO dialog_act1 VALUES (6, "I love movies like this. They are amazing. I wish I could see more like this in these days.", 7);
+INSERT INTO dialog_act1 VALUES (3, "What do you think is the better movie? Its not cliche to ask if its Gone with the Wind or Wizard of Oz at all.", 6);
 
 
 #Dialog acts2 table commands
-INSERT INTO dialog_act2 VALUES (7, "I think realism in today's films kind of make them bland. I think that's why Avatar was so popular,  because of how vibrant the characters were,  from a color standpoint.", 5);
-INSERT INTO dialog_act2 VALUES (2, "The transition from black and white to color in Wizard of Oz really did draw the audience into the world of Oz. After reading the books,  I think this was probably the most profound way to do that in film.", 5);
-INSERT INTO dialog_act2 VALUES (2, "You know,  say what you will about how they revolutionized the film industry,  but in both cases the books were better.", 2);
-INSERT INTO dialog_act2 VALUES (9, "There is a reason everyone knows who Judy Garland is,  that's for sure.", 3);
 INSERT INTO dialog_act2 VALUES (13, "I've never heard as much laughter in a movie theater as I did for that picture.", 5);
-INSERT INTO dialog_act2 VALUES (7, "That's the great thing about technicolor though! It's almost like modern art on film. The vibrancy is what makes it special.", 3);
-INSERT INTO dialog_act2 VALUES (10, "Well,  in the case of ""Gone with the Wind"" it was the first major motion picture to have a swear word in it.", 3);
-INSERT INTO dialog_act2 VALUES (9, "Isn't it crazy to think they were being filmed at the same time?", 5);
-INSERT INTO dialog_act2 VALUES (7, "We always joke that you recognize an MGM musical more from the color than the soundtrack.", 4);
-INSERT INTO dialog_act2 VALUES (9, "Did you know at the time there was only two color cameras for shooting owned by the studio?", 2);
-INSERT INTO dialog_act2 VALUES (6, "Do you remember the Carol Burnett Gone With the Wind sketch? Vicki Lawrence was pure gold in that.", 2);
-INSERT INTO dialog_act2 VALUES (2, "You mean other than how to stir up controversy?", 4);
+INSERT INTO dialog_act2 VALUES (7, "I think realism in today's films kind of make them bland. I think that's why Avatar was so popular,  because of how vibrant the characters were,  from a color standpoint.", 5);
 INSERT INTO dialog_act2 VALUES (9, "Dorothy who?", 4);
+INSERT INTO dialog_act2 VALUES (7, "That's the great thing about technicolor though! It's almost like modern art on film. The vibrancy is what makes it special.", 3);
+INSERT INTO dialog_act2 VALUES (9, "There is a reason everyone knows who Judy Garland is,  that's for sure.", 3);
+INSERT INTO dialog_act2 VALUES (2, "You mean other than how to stir up controversy?", 4);
+INSERT INTO dialog_act2 VALUES (2, "You know,  say what you will about how they revolutionized the film industry,  but in both cases the books were better.", 2);
+INSERT INTO dialog_act2 VALUES (7, "We always joke that you recognize an MGM musical more from the color than the soundtrack.", 4);
+INSERT INTO dialog_act2 VALUES (6, "Do you remember the Carol Burnett Gone With the Wind sketch? Vicki Lawrence was pure gold in that.", 2);
+INSERT INTO dialog_act2 VALUES (9, "Did you know at the time there was only two color cameras for shooting owned by the studio?", 2);
+INSERT INTO dialog_act2 VALUES (10, "Well,  in the case of ""Gone with the Wind"" it was the first major motion picture to have a swear word in it.", 3);
+INSERT INTO dialog_act2 VALUES (2, "The transition from black and white to color in Wizard of Oz really did draw the audience into the world of Oz. After reading the books,  I think this was probably the most profound way to do that in film.", 5);
+INSERT INTO dialog_act2 VALUES (9, "Isn't it crazy to think they were being filmed at the same time?", 5);
 
 
 #Dialog acts3 table commands
-INSERT INTO dialog_act3 VALUES (8, "I think all of the Monty Python moves are perfect for a game night.", 2);
-INSERT INTO dialog_act3 VALUES (2, "Remember when Harry Potter came out on film? I never liked the films near as much as I did the books. How can you fit a hole long book into an hour and a half?", 5);
-INSERT INTO dialog_act3 VALUES (7, "Talking about fast paced movies,  I can't stand the animated movies that come out today. Call me slow but there's no way that I can keep track of all the stuff that's happening on screen.", 2);
+INSERT INTO dialog_act3 VALUES (4, "Yeah,  there was something so amazing about the witches and all of the different colors and the ruby slippers.", 5);
 INSERT INTO dialog_act3 VALUES (9, "Well that's true. I remember ""bring forth the shrubbery"" the best.", 3);
+INSERT INTO dialog_act3 VALUES (8, "I think all of the Monty Python moves are perfect for a game night.", 2);
 INSERT INTO dialog_act3 VALUES (1, "I think it's amazing how memorable any transition is with technology,  it's like we'll always remember the very first smart phone,  the first laptop,  tablet,  3D movie... The Wizard of Oz was the first of it's kind just like those,  which is what made it so amazing.", 2);
 INSERT INTO dialog_act3 VALUES (5, "It's like watching reality in fast forward what with all of the action scenes so close together.", 4);
-INSERT INTO dialog_act3 VALUES (6, "Yeah it's just so ""profound"" how sexist and degrading that movie is.", 4);
-INSERT INTO dialog_act3 VALUES (3, "I agree,  I loved seeing her step from her house into the colorful world... it was mesmerizing.", 3);
 INSERT INTO dialog_act3 VALUES (0, "I am in total agreement there. In most cases the movies pale in comparison to the books.", 3);
-INSERT INTO dialog_act3 VALUES (4, "Yeah,  there was something so amazing about the witches and all of the different colors and the ruby slippers.", 5);
+INSERT INTO dialog_act3 VALUES (6, "Yeah it's just so ""profound"" how sexist and degrading that movie is.", 4);
+INSERT INTO dialog_act3 VALUES (2, "Remember when Harry Potter came out on film? I never liked the films near as much as I did the books. How can you fit a hole long book into an hour and a half?", 5);
+INSERT INTO dialog_act3 VALUES (7, "Talking about fast paced movies,  I can't stand the animated movies that come out today. Call me slow but there's no way that I can keep track of all the stuff that's happening on screen.", 2);
+INSERT INTO dialog_act3 VALUES (3, "I agree,  I loved seeing her step from her house into the colorful world... it was mesmerizing.", 3);
 
 
 #Dialog acts4 table commands
-INSERT INTO dialog_act4 VALUES (6, "Did you see the new Oz movie they made with James Franco?", 2);
-INSERT INTO dialog_act4 VALUES (12, "Monty Python some how can reach fifteen year old kids,  decades after its release and at the same time make a posh fifty year old laugh just as loud.", 5);
-INSERT INTO dialog_act4 VALUES (2, "I think that even films that revolutionize the industry can still pale in how great they are,  when compared to the book they come from.", 3);
+INSERT INTO dialog_act4 VALUES (5, "Yeah,  although Dorothy herself seemed pretty mellow about it.", 4);
+INSERT INTO dialog_act4 VALUES (9, "Books have a larger scope to show a story in though. They have as much or as little time as they need,  no constraints on budgets when it comes to a fantasy world or effects. Take Harry Potter,  the movies were huge,  effects,  just the whole thing all of them,  massive. But the books were somehow bigger.", 5);
+INSERT INTO dialog_act4 VALUES (13, "That scene is funny. I really like all of the Monty Plython movies though. And the other ones are funny,  but not as well known as the Holy Grail.", 2);
+INSERT INTO dialog_act4 VALUES (3, "The makeup was really good in The Wizard of Oz,  too. I mean it's not like modern horror movie stuff,  but the Scarecrow was really very effective,  especially for the time.", 5);
 INSERT INTO dialog_act4 VALUES (1, "Do you remember how we used to get around in strange cities before we all had smartphones?", 2);
+INSERT INTO dialog_act4 VALUES (12, "Monty Python some how can reach fifteen year old kids,  decades after its release and at the same time make a posh fifty year old laugh just as loud.", 5);
+INSERT INTO dialog_act4 VALUES (10, "Right. COLOR TV! It's the FUTURE!", 4);
+INSERT INTO dialog_act4 VALUES (7, "I know. I mean,  the special effects in both those movies seem really hokey now,  but I'm sure they were exciting at the time. Do you remember how cool we thought the Internet was when it first came out in the early 90s?", 3);
+INSERT INTO dialog_act4 VALUES (2, "I think that even films that revolutionize the industry can still pale in how great they are,  when compared to the book they come from.", 3);
+INSERT INTO dialog_act4 VALUES (6, "Did you see the new Oz movie they made with James Franco?", 2);
+INSERT INTO dialog_act4 VALUES (14, "I just can't get that joke. My humor needs to be actually funny,  you know?", 4);
+INSERT INTO dialog_act4 VALUES (8, "I agree. I wonder how many people had access to color screens at the time,  though. Or did that matter?", 5);
 INSERT INTO dialog_act4 VALUES (0, "Big magic,  fantasy books are hard to condense. Take Lord of the Rings,  the whole world could never have fit into just a couple books. It is really its own reality.", 2);
 INSERT INTO dialog_act4 VALUES (4, "And it may not have been especially innovative,  but making The Wizard of Oz a musical was a really smart choice. They could use that to communicate the emotion much better than a straightforward movie.", 3);
 INSERT INTO dialog_act4 VALUES (11, "I'm pretty sure that is because Rowling used magic to condense that world down into pages,  stuff you muggles would not understand.", 4);
-INSERT INTO dialog_act4 VALUES (8, "I agree. I wonder how many people had access to color screens at the time,  though. Or did that matter?", 5);
-INSERT INTO dialog_act4 VALUES (5, "Yeah,  although Dorothy herself seemed pretty mellow about it.", 4);
-INSERT INTO dialog_act4 VALUES (13, "That scene is funny. I really like all of the Monty Plython movies though. And the other ones are funny,  but not as well known as the Holy Grail.", 2);
-INSERT INTO dialog_act4 VALUES (14, "I just can't get that joke. My humor needs to be actually funny,  you know?", 4);
-INSERT INTO dialog_act4 VALUES (7, "I know. I mean,  the special effects in both those movies seem really hokey now,  but I'm sure they were exciting at the time. Do you remember how cool we thought the Internet was when it first came out in the early 90s?", 3);
-INSERT INTO dialog_act4 VALUES (9, "Books have a larger scope to show a story in though. They have as much or as little time as they need,  no constraints on budgets when it comes to a fantasy world or effects. Take Harry Potter,  the movies were huge,  effects,  just the whole thing all of them,  massive. But the books were somehow bigger.", 5);
-INSERT INTO dialog_act4 VALUES (10, "Right. COLOR TV! It's the FUTURE!", 4);
-INSERT INTO dialog_act4 VALUES (3, "The makeup was really good in The Wizard of Oz,  too. I mean it's not like modern horror movie stuff,  but the Scarecrow was really very effective,  especially for the time.", 5);
 
 
 #Mapping commands
